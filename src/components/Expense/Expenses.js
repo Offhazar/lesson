71f205269c12317ac914b2bb3496f04c0b13b433
Almazbek uulu Adilet <aduletalmaz@gmail.com>
2021-10-23 17:36:32 +0300

    2

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -10,35 +10,36 @@ function Expenses(props) {
 
   const filterChangeHundler = (selectYear) => {
     console.log(selectYear);
+    setfilteredYear(selectYear);
   };
   return (
     <Card className="expenses">
-      <ExpensesFilter onChangeFilter={filterChangeHundler} />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      {/* {props.expenses.map((el, id) => {
+      {
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHundler}
+        />
+        // <ExpenseItem
+        //   title={props.expenses[0].title}
+        //   amount={props.expenses[0].amount}
+        //   date={props.expenses[0].date}
+        // />
+        // <ExpenseItem
+        //   title={props.expenses[1].title}
+        //   amount={props.expenses[1].amount}
+        //   date={props.expenses[1].date}
+        // />
+        // <ExpenseItem
+        //   title={props.expenses[2].title}
+        //   amount={props.expenses[2].amount}
+        //   date={props.expenses[2].date}
+        // /> */}
+      }
+      {props.expenses.map((el) => {
         return (
-          <ExpenseItem
-            title={el.title}
-            amount={el.amount}
-            date={el.date}
-            key={id}
-          />
+          <ExpenseItem title={el.title} amount={el.amount} date={el.date} />
         );
-      })} */}
+      })}
     </Card>
   );
 }
