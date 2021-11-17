ced5aeb4cea12932504a3fa0525ae5d26c297f91
Almazbek uulu Adilet <aduletalmaz@gmail.com>
2021-11-17 16:22:18 +0300

    f

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -23,45 +23,65 @@ const emailReducer = (preveState, action) => {
   };
 };
 
+const passwordReducer = (preveState, action) => {
+  if (action.type === 'USER_INPUT') {
+    return {
+      value: action.value,
+      isValid: action.value.trim().length > 6,
+    };
+  }
+  if (action.type === 'INPUT_WARNING') {
+    return {
+      value: preveState.value,
+      isValid: preveState.value.trim().length > 6,
+    };
+  }
+  return {
+    value: '',
+    isValid: false,
+  };
+};
+
 const Login = (props) => {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '',
     isValid: false,
   });
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
-  const [formIsValid, setFormIsValid] = useState(false);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     console.log('fdsfds');
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
-  //   }, 3000);
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: '',
+    isValid: false,
+  });
 
-  //   return () => {
-  //     console.log('cleaun up');
-  //     clearTimeout(timer);
-  //   };
-  // }, [enteredEmail, enteredPassword]);
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('fdsfds');
+      setFormIsValid(emailState.isValid && passwordState.isValid);
+    }, 3000);
+
+    return () => {
+      console.log('cleaun up');
+      clearTimeout(timer);
+    };
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: 'USER_INPUT', value: event.target.value });
     // setEnteredEmail(event.target.value);
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && emailState.value.includes('@')
-    );
+    // setEnteredPassword(event.target.value);
+    dispatchPassword({ type: 'USER_INPUT', value: event.target.value });
   };
 
   const validateEmailHandler = () => {
@@ -70,12 +90,13 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    // setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({ type: 'INPUT_WARNING' });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -97,14 +118,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../../store/aut-context';
 
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
+  const ctxData = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {ctxData.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctxData.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctxData.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctxData.onLogout}>Logout</button>
           </li>
         )}
       </ul>
